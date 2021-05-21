3476d84c359515497dabdaa1d2e614363cf33d1a
Felipe Apablaza <felipe.apablaza@protonmail.com>
2021-05-20 20:57:47 -0400

    [macOS] changed implementation frontend language to Swift

---
+++ modules/Xenoide.Gui.Cocoa/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Xenoide
+//
+//  Created by Felipe Apablaza on 18-05-21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
