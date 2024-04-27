2f47b70ff686b58e338ee6dfda57e2802f6efd36
Felipe Apablaza <felipe.apablaza@protonmail.com>
2024-04-27 15:15:00 -0400

    setup the main window

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     QMainWindow mainWindow;
+
+    mainWindow.setWindowTitle("Xenoide");
+    mainWindow.resize(800, 600);
     mainWindow.show();
 
     return app.exec();
