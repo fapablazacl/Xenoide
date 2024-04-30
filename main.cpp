0487fca8cdd67d58800993d1e95a02a7d89175f1
Felipe Apablaza <felipe.apablaza@protonmail.com>
2024-04-29 21:34:55 -0400

    setted current qt6 version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+
 #include <QApplication>
 #include <QMainWindow>
 
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
     QMainWindow mainWindow;
 
     mainWindow.setWindowTitle("Xenoide");
